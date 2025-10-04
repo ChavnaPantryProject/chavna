785038aabb5d82f8e7f4240e55b7872c2c9f877b
BPAmsler <manof2words@yahoo.com>
2025-10-04 12:40:16 -0700

    finished login authentication

--- backend/pantryproject/src/main/java/com/chavna/pantryproject/HelloController.java
+++ backend/pantryproject/src/main/java/com/chavna/pantryproject/HelloController.java
@@ -1,8 +1,11 @@
 package com.chavna.pantryproject;
 
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.apache.coyote.BadRequestException;
@@ -10,6 +13,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder.BCryptVersion;
 import org.springframework.security.web.firewall.RequestRejectedException;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ObjectError;
@@ -24,24 +29,35 @@ import jakarta.validation.constraints.NotNull;
 @RestController
 public class HelloController {
     private static Logger logger = LoggerFactory.getLogger(HelloController.class);
+    private static final String USERS_TABLE = "users";
 
     private static Connection getRemoteConnection() {
-        if (System.getenv("RDS_HOSTNAME") != null) {
-            try {
-                Class.forName("org.postgresql.Driver");
+        try {
+            Class.forName("org.postgresql.Driver");
+            String use = System.getenv("USE_NEON_DATABASE");
+            System.err.println("USE_NEON_DATABASE=\"" + use + "\"");
+            if (use != null && use.equals("1")) {
+                String jdbcUrl = "jdbc:" + System.getenv("DATABASE_URL");
+                return DriverManager.getConnection(jdbcUrl);
+            }
+                
+            String hostname = System.getenv("RDS_HOSTNAME");
+            if (hostname != null) {
                 String dbName = System.getenv("RDS_DB_NAME");
                 String userName = System.getenv("RDS_USERNAME");
                 String password = System.getenv("RDS_PASSWORD");
-                String hostname = System.getenv("RDS_HOSTNAME");
                 String port = System.getenv("RDS_PORT");
                 String jdbcUrl = "jdbc:postgresql://" + hostname + ":" + port + "/" + dbName + "?user=" + userName + "&password=" + password;
-                Connection con = DriverManager.getConnection(jdbcUrl);
-                return con;
+                return DriverManager.getConnection(jdbcUrl);
             }
-            catch (ClassNotFoundException e) { logger.warn(e.toString());}
-            catch (SQLException e) { logger.warn(e.toString());}
         }
-        return null;
+        // Rethrow exceptions as RuntimeExceptions since spring boot is going to automatically catch them anyway.
+        // I'd rather not be forced to explicitly deal with them if they arent going to crash the whole server.
+        // Probably bad practice, but idc.
+        catch (RuntimeException ex) { throw ex; }
+        catch (Exception ex) { throw new RuntimeException(ex); }
+
+        throw new RuntimeException("nothing happened.");
     }
 
     public static class LoginRequest {
@@ -52,12 +68,27 @@ public class HelloController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@Valid @RequestBody LoginRequest request, Errors errors) throws BadRequestException {
+    public ResponseEntity<String> login(@Valid @RequestBody LoginRequest request, Errors errors) throws BadRequestException, SQLException {
         if (errors.hasErrors())
             return ResponseEntity.badRequest().body(errors.getAllErrors().get(0).toString());
         
+        var con = getRemoteConnection();
 
-        return ResponseEntity.ok("Login request: " + request.email + ", " + request.password);
+        PreparedStatement statement = con.prepareStatement("SELECT password_hash FROM " + USERS_TABLE + " where email = ?");
+        statement.setString(1, request.email);
+        ResultSet results = statement.executeQuery();
+
+        if (results.next()) {
+            byte[] hash = results.getBytes(1);
+
+            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(BCryptVersion.$2B, 8);
+            if (encoder.matches(request.password, new String(hash, StandardCharsets.UTF_8))) {
+
+                return ResponseEntity.ok("Sucessful Login.");
+            }
+        }
+
+        return ResponseEntity.ok("Invalid Login Credentials.");
     }
 
 }
