447cfe933a3025868044f32daa69d272d52aff2a
BPAmsler <manof2words@yahoo.com>
2025-10-04 13:56:37 -0700

    created get-account mapping

--- backend/pantryproject/src/main/java/com/chavna/pantryproject/UserController.java
+++ backend/pantryproject/src/main/java/com/chavna/pantryproject/UserController.java
@@ -1,6 +1,5 @@
 package com.chavna.pantryproject;
 
-import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -11,24 +10,26 @@ import java.sql.SQLException;
 import org.apache.coyote.BadRequestException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder.BCryptVersion;
-import org.springframework.security.web.firewall.RequestRejectedException;
 import org.springframework.validation.Errors;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 
 @RestController
-public class HelloController {
-    private static Logger logger = LoggerFactory.getLogger(HelloController.class);
+public class UserController {
+    private static Logger logger = LoggerFactory.getLogger(UserController.class);
     private static final String USERS_TABLE = "users";
 
     private static Connection getRemoteConnection() {
@@ -68,9 +69,9 @@ public class HelloController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@Valid @RequestBody LoginRequest request, Errors errors) throws BadRequestException, SQLException {
+    public void login(@Valid @RequestBody LoginRequest request, Errors errors) throws BadRequestException, SQLException {
         if (errors.hasErrors())
-            return ResponseEntity.badRequest().body(errors.getAllErrors().get(0).toString());
+           throw new ResponseStatusException(HttpStatus.BAD_REQUEST, errors.getAllErrors().get(0).toString());
         
         var con = getRemoteConnection();
 
@@ -84,11 +85,72 @@ public class HelloController {
             BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(BCryptVersion.$2B, 8);
             if (encoder.matches(request.password, new String(hash, StandardCharsets.UTF_8))) {
 
-                return ResponseEntity.ok("Sucessful Login.");
+                throw new ResponseStatusException(HttpStatus.OK, "Sucessful Login.");
             }
         }
 
-        return ResponseEntity.ok("Invalid Login Credentials.");
+        throw new ResponseStatusException(HttpStatus.OK, "Invalid Login Credentials.");
     }
 
+    public static class UserExistsRequest {
+        @NotNull
+        public String email;
+    }
+
+    @GetMapping("/user-exists")
+    public void userExists(@Valid @RequestBody UserExistsRequest request, Errors errors) throws SQLException {
+        if (errors.hasErrors())
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, errors.getAllErrors().get(0).toString());
+
+        var con = getRemoteConnection();
+
+        PreparedStatement statement = con.prepareStatement("SELECT 1 FROM " + USERS_TABLE + " where email = ?");
+        statement.setString(1, request.email);
+        ResultSet results = statement.executeQuery();
+
+        if (results.next())
+            throw new ResponseStatusException(HttpStatus.OK, Boolean.toString(results.getInt(1) == 1));
+
+        throw new ResponseStatusException(HttpStatus.OK, "false");
+    }
+
+    public static class CreateAccountRequest {
+        @NotNull
+        public String email;
+        @NotNull
+        public String password;
+    }
+
+    @PostMapping("/create-account")
+    public void createAccount(@Valid @RequestBody CreateAccountRequest request, Errors errors) throws SQLException {
+        if (errors.hasErrors())
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, errors.getAllErrors().get(0).toString());
+        
+        var con = getRemoteConnection();
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(BCryptVersion.$2B, 8);
+        String hash = encoder.encode(request.password);
+
+        try {
+            PreparedStatement statement = con.prepareStatement(String.format(
+            """
+                INSERT INTO %s (email, password_hash)
+                    VALUES (?, ?)
+            """, USERS_TABLE));
+            statement.setString(1, request.email);
+            statement.setBytes(2, hash.getBytes());
+            statement.executeUpdate();
+        }
+        catch (SQLException ex) {
+            if (ex.getSQLState().equals("23505"))
+                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "User with email already exists.");
+                // return ResponseEntity.internalServerError().body("User with email already exists.");
+
+            throw ex;
+        }
+
+        // This feels wrong, but I can't find another way to get an
+        // auto generated JSON response
+        throw new ResponseStatusException(HttpStatus.OK, "Account created succesfully");
+    }
 }
