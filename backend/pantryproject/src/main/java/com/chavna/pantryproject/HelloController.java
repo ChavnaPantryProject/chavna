c1a45570493e7e9396bf603dc0ff844a657e9332
BPAmsler <manof2words@yahoo.com>
2025-10-03 21:52:33 -0700

    worked out login request

--- backend/pantryproject/src/main/java/com/chavna/pantryproject/HelloController.java
+++ backend/pantryproject/src/main/java/com/chavna/pantryproject/HelloController.java
@@ -1,13 +1,25 @@
 package com.chavna.pantryproject;
 
+import java.net.http.HttpResponse;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import org.apache.coyote.BadRequestException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.web.firewall.RequestRejectedException;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 
 @RestController
 public class HelloController {
@@ -15,28 +27,37 @@ public class HelloController {
 
     private static Connection getRemoteConnection() {
         if (System.getenv("RDS_HOSTNAME") != null) {
-        try {
-            Class.forName("org.postgresql.Driver");
-            String dbName = System.getenv("RDS_DB_NAME");
-            String userName = System.getenv("RDS_USERNAME");
-            String password = System.getenv("RDS_PASSWORD");
-            String hostname = System.getenv("RDS_HOSTNAME");
-            String port = System.getenv("RDS_PORT");
-            String jdbcUrl = "jdbc:postgresql://" + hostname + ":" + port + "/" + dbName + "?user=" + userName + "&password=" + password;
-            logger.trace("Getting remote connection with connection string from environment variables.");
-            Connection con = DriverManager.getConnection(jdbcUrl);
-            logger.info("Remote connection successful.");
-            return con;
-        }
+            try {
+                Class.forName("org.postgresql.Driver");
+                String dbName = System.getenv("RDS_DB_NAME");
+                String userName = System.getenv("RDS_USERNAME");
+                String password = System.getenv("RDS_PASSWORD");
+                String hostname = System.getenv("RDS_HOSTNAME");
+                String port = System.getenv("RDS_PORT");
+                String jdbcUrl = "jdbc:postgresql://" + hostname + ":" + port + "/" + dbName + "?user=" + userName + "&password=" + password;
+                Connection con = DriverManager.getConnection(jdbcUrl);
+                return con;
+            }
             catch (ClassNotFoundException e) { logger.warn(e.toString());}
             catch (SQLException e) { logger.warn(e.toString());}
         }
         return null;
     }
 
-	@GetMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot!";
-	}
+    public static class LoginRequest {
+        @NotNull
+        public String email;
+        @NotNull
+        public String password;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@Valid @RequestBody LoginRequest request, Errors errors) throws BadRequestException {
+        if (errors.hasErrors())
+            return ResponseEntity.badRequest().body(errors.getAllErrors().get(0).toString());
+        
+
+        return ResponseEntity.ok("Login request: " + request.email + ", " + request.password);
+    }
 
 }
