3b88864d309ee69fa72092e21b233da4cf4cf22c
Brian Amsler <manof2words@yahoo.com>
2025-10-29 11:52:44 -0700

    family request shows name (#139)

--- backend/pantryproject/src/main/java/com/chavna/pantryproject/Database.java
+++ backend/pantryproject/src/main/java/com/chavna/pantryproject/Database.java
@@ -7,8 +7,17 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 public class Database {
+    public static final String USERS_TABLE = "users";
+    public static final String PERSONAL_INFO_TABLE = "personal_info";
+    public static final String FAMILY_TABLE = "family";
+    public static final String FAMILY_MEMBER_TABLE = "family_member";
+
     public static Connection getRemoteConnection() {
         try {
             Class.forName("org.postgresql.Driver");
@@ -49,12 +58,13 @@ public class Database {
     public static HashMap<String, Object> getDefaultTableEntry(Connection dbConnection, String tableName) throws SQLException {
         HashMap<String, Object> object = new HashMap<>();
 
-        PreparedStatement statement = dbConnection.prepareStatement("""
+        PreparedStatement statement = dbConnection.prepareStatement(
+            """
             SELECT column_name, column_default, data_type
             FROM information_schema.columns
             WHERE (table_schema, table_name) = ('public', ?)
             ORDER BY ordinal_position;
-        """);
+            """);
         statement.setString(1, tableName);
         ResultSet result = statement.executeQuery();
 
@@ -65,9 +75,10 @@ public class Database {
 
             // Schema stores default values as strings, so it must be cast to the correct type.
             // Unfortunately, I can't find a good way to do this in java rather than an sql query for each field.
-            PreparedStatement castStatement = dbConnection.prepareStatement(String.format("""
+            PreparedStatement castStatement = dbConnection.prepareStatement(String.format(
+                """
                 SELECT CAST(casted_value AS %s) FROM (VALUES (?)) AS temp_values (casted_value)
-            """, data_type));
+                """, data_type));
             castStatement.setString(1, default_value);
             ResultSet casted = castStatement.executeQuery();
 
@@ -76,5 +87,40 @@ public class Database {
         }
 
         return object;
+    }
+
+    public static HashMap<String, Object> getUserPersonalInfo(Connection con, UUID user) throws SQLException {
+        PreparedStatement personalInfoStatement = con.prepareStatement(String.format(
+            """
+            SELECT * FROM %s WHERE user_id = ?
+            """, PERSONAL_INFO_TABLE));
+        personalInfoStatement.setObject(1, user);
+        ResultSet query2 = personalInfoStatement.executeQuery();
+        
+        HashMap<String, Object> jsonObject;
+        if (query2.next()) {
+            jsonObject = Database.objectFromResultSet(query2);
+        } else {
+            jsonObject = Database.getDefaultTableEntry(con, PERSONAL_INFO_TABLE);
+        }
+        
+        jsonObject.remove("user_id");
+
+        return jsonObject;
+    }
+
+    public static String getUserEmail(Connection con, UUID user) throws SQLException {
+        PreparedStatement query = con.prepareStatement(String.format(
+            """
+            SELECT email FROM %s
+            WHERE id = ?
+            """, USERS_TABLE));
+        query.setObject(1, user);
+        ResultSet result = query.executeQuery();
+
+        if (!result.next())
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User does not exist.");
+        
+        return result.getString(1);
     }
 }
