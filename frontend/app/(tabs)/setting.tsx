3157f422a6ca7a31b7c60b3bea65786c0c0b60e2
nhatthanh7890 <nhatthanh7890@gmail.com>
2025-10-30 12:59:47 -0700

    final household update

--- frontend/app/(tabs)/setting.tsx
+++ frontend/app/(tabs)/setting.tsx
@@ -350,7 +350,7 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     alignItems: "center",
     marginBottom: 20,
-    marginTop: 50,
+    marginTop: 0,
   },
   headerTitle: {
     fontSize: 18,
