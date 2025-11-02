7b2fb855a719773d696133f6c2fd40db1a2b9689
Jonathan Diaz <jonathandiaz@MacBookPro.lan>
2025-11-02 15:42:29 -0800

    refined minor details

--- frontend/app/meals/mealinfo.tsx
+++ frontend/app/meals/mealinfo.tsx
@@ -1,8 +1,9 @@
 // meal nutrition facts screen
 // make sure to add the three dots at the top right corner to enter edit mode/to delete meal
 // "go to ingredients" at the bottom right corner
+// the. nurtirion will update depending on what ingredients were inputted
 
-import React from "react";
+import React, {useState} from "react";
 import { 
     View, 
     Text, 
@@ -10,7 +11,28 @@ import {
     TouchableOpacity, 
     FlatList, 
     StyleSheet,
-    SafeAreaView
+    SafeAreaView,
+    ScrollView,
+    Modal,
+    Pressable,
 } from "react-native";
 import { useRouter } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
+
+export default function MealNutritionScreen() {
+    const router = useRouter();
+    const [menuVisible, setMenuVisible] = useState(false);
+
+    return (
+        <SafeAreaView style={styles.container}>
+
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#FFFFFF",
+    },
+})
