08cc62eec5ac1d37b42e20a4ad4b1c403df15fdc
nhatthanh7890 <45188988+nhatthanh7890@users.noreply.github.com>
2025-11-03 11:02:24 -0800

    Revert "Daniella/meal nutrition (#146)"
    This reverts commit a831f3d3d35b0ad610d3761475b60bd9a492a282.

--- frontend/app/meals/_layout.tsx
+++ frontend/app/meals/_layout.tsx
@@ -1,29 +1,11 @@
 // app/meal/_layout.tsx
 
-import { Slot, Stack } from "expo-router";
+import { Slot } from "expo-router";
 
 export const unstable_settings = {
     tabBarStyle: { display: "none"},
 };
 
 export default function MealLayout() {
-    return (
-        <Stack
-            screenOptions={{
-                headerShown: false, // keep custom headers
-                animation: "slide_from_right", // default animation for forward navigation
-            }}
-        >
-            {/* define transitions for each screen */}
-            <Stack.Screen
-                name="meal"
-                options={{
-                    animation: "slide_from_left", // when navigating back
-                }}
-            />
-            <Stack.Screen name="meal_ingredient" />
-            <Stack.Screen name="editmeal" />
-            <Stack.Screen name="mealinfo" />
-        </Stack>
-    );
+    return <Slot />;
 }
